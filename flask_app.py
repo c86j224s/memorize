170281d3ac214c8f741cbca25e8766bcb9be8cf3
c86j224s <c86j224s@gmail.com>
2013-09-29 23:46:42 +0900

    delete template/_global_navi.html

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -16,3 +16,6 @@ def about ():
 			('http://www.google.com', 'Google!'), 
 			('http://www.naver.com', 'Naver!'),
 			('http://www.daum.net', 'Daum!')))
+
+app.debug = True
+app.run ()
